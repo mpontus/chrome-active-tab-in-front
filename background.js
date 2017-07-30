9c4b54594c9407e30bd1743f3cc2c12943da48f3
Michael Pontus <m.pontus@gmail.com>
2017-07-30 17:23:15 +0300

    Some refactor.

--- background.js
+++ background.js
@@ -1,7 +1,13 @@
+// How often should we reattempt movign the tab if it fails due to known error?
 const MOVE_REATTEMPT_INTERVAL = 17;
+
+// Hardcoded message of the known error. Better workaround is unknown.
 const ERROR_MESSAGE_MOVE_WHILE_DRAGGING =
   'Tabs cannot be edited right now (user may be dragging a tab).';
 
+/**
+ * Returns currently active tab in active window.
+ */
 function getActiveTab(cb) {
   chrome.windows.getCurrent(({ id: windowId }) => {
     chrome.tabs.query({ active: true, windowId }, (([activeTab]) => {
@@ -10,6 +16,9 @@ function getActiveTab(cb) {
   });
 }
 
+/**
+ * Returns tab at the given index in active window.
+ */
 function getTabByIndex(index, cb) {
   chrome.windows.getCurrent(({ windowId }) => {
     chrome.tabs.query({ index, windowId }, (([activeTab]) => {
@@ -18,6 +27,9 @@ function getTabByIndex(index, cb) {
   });
 }
 
+/**
+ * Ensure that active tab is at the front of all others in active window.
+ */
 function ensureActiveTabInFront() {
   getActiveTab((tab) => {
     if (tab.index !== 0) {
@@ -38,14 +50,15 @@ function ensureActiveTabInFront() {
   })
 }
 
-chrome.tabs.onActivated.addListener(() => {
-  ensureActiveTabInFront();
-});
+// Callback is invoked when different tab is activated
+chrome.tabs.onActivated.addListener(ensureActiveTabInFront);
 
-chrome.tabs.onMoved.addListener(() => {
-  ensureActiveTabInFront();
-});
+// Callback is invoked when active tab changes places
+chrome.tabs.onMoved.addListener(ensureActiveTabInFront);
 
+/**
+ * Move current tab at the end of the list before activating next tab.
+ */
 chrome.commands.onCommand.addListener((command) => {
   switch (command) {
     case 'bury-tab': {
@@ -53,7 +66,10 @@ chrome.commands.onCommand.addListener((command) => {
         getTabByIndex(1, (nextTab) => {
           if (!nextTab) return;
 
+          // Move first tab (which is active) all the way to the back
           chrome.tabs.move(currentTab.id, { index: -1 });
+
+          // Activate the second (now first) tab
           chrome.tabs.update(nextTab.id, { active: true });
         });
       });
